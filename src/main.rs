19870283f6fd2a4b4d83338309f4de152441bb0e
Herman Venter <hermanv@fb.com>
2018-11-28 16:00:26 -0800

    Add MIRAI specific logging support. Issue #4.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
-
-#![feature(box_syntax)]
+//
 // In an ideal world there would be a stable well documented set of crates containing a specific
 // version of the Rust compiler along with its sources and debug information. We'd then just get
 // those from crate.io and merely go on our way as just another Rust application. Rust compiler
@@ -14,34 +13,47 @@
 // private and not very stable set of APIs from whatever compiler is in the path when we run Mirai.
 // While pretty bad, it is a lot less bad than having to write our own compiler, so here goes.
 #![feature(rustc_private)]
+#![feature(box_syntax)]
 
+extern crate env_logger;
 extern crate mirai;
 extern crate rustc_driver;
 
 use mirai::callbacks;
 use mirai::utils;
+use std::env;
 use std::path::Path;
 
 #[cfg_attr(tarpaulin, skip)]
 fn main() {
     rustc_driver::run(|| {
-        rustc_driver::init_rustc_env_logger();
-        let mut command_line_arguments: Vec<_> = std::env::args().collect();
+        // Initialize loggers.
+        if env::var("RUST_LOG").is_ok() {
+            rustc_driver::init_rustc_env_logger();
+        }
+        if env::var("MIRAI_LOG").is_ok() {
+            let e = env_logger::Env::new()
+                .filter("MIRAI_LOG")
+                .write_style("MIRAI_LOG_STYLE");
+            env_logger::init_from_env(e);
+        }
+
+        // Get command line arguments from environment and massage them a bit.
+        let mut command_line_arguments: Vec<_> = env::args().collect();
 
         // Setting RUSTC_WRAPPER causes Cargo to pass 'rustc' as the first argument.
-        // We're invoking the compiler programmatically, so we ignore this.
+        // We're invoking the compiler programmatically, so we remove it if present.
         if command_line_arguments.len() > 1
             && Path::new(&command_line_arguments[1]).file_stem() == Some("rustc".as_ref())
         {
-            // we still want to be able to invoke it normally though
             command_line_arguments.remove(1);
         }
 
         // Tell compiler where to find the std library and so on.
-        // Have to do this here because the standard rustc driver does it and we are replacing it.
+        // The compiler relies on the standard rustc driver to tell it, so we have to do likewise.
         command_line_arguments.push(String::from("--sysroot"));
         command_line_arguments.push(utils::find_sysroot());
-        // todo figure out how and where summaries will be written to
+
         rustc_driver::run_compiler(
             &command_line_arguments,
             box callbacks::MiraiCallbacks::default(),
