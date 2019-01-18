9df1196a4568d52e826e05fc480f2259ddc37bcf
Herman Venter <hermanv@fb.com>
2019-01-17 18:30:42 -0800

    Bundle DefId value with function constant to help with summary caching.

---
+++ tests/run-pass/func_call.rs
@@ -0,0 +1,12 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that uses a function summary.
+
+fn foo() -> i32 { 123 }
+pub fn main() {
+    let _x = foo();
+}
