ecf43079b9114254dd135a8f321bc03dcaf39ea0
Herman Venter <hermanv@fb.com>
2019-01-26 15:08:58 -0800

    Array access in context needing unwind with cleanup.

---
+++ tests/run-pass/array_access_with_unwind.rs
@@ -0,0 +1,14 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that needs to do cleanup if an array access is out of bounds.
+
+pub fn foo(arr: &mut [i32], i: usize) -> String {
+    arr[i] = 123;
+    let result = String::from("foo");
+    let _e = arr[i];
+    result
+}
