5eb1c20c1473b1943f950e4e7beb9ffa1a4fd259
Herman Venter <hermanv@fb.com>
2019-02-06 13:29:19 -0800

    Deal with array sub slice patterns. (#59)

--- tests/run-pass/array_pattern_move.rs
+++ tests/run-pass/array_pattern_move.rs
@@ -9,11 +9,22 @@
 #![feature(box_syntax)]
 #![feature(slice_patterns)]
 
-pub fn subslice_pattern_from_end(arg: bool) {
-    let a = [String::from("a"), String::from("a"), String::from("a")];
-    if arg {
-        let[.., _x, _] = a;
+pub fn main() {
+    array_pattern(true);
+    array_pattern(false);
+}
+
+struct Foo {
+    f: i32,
+}
+
+pub fn array_pattern(from_end: bool) {
+    let a = [Foo {f: 10}, Foo {f: 20}, Foo {f: 30}, Foo { f: 40 }];
+    if from_end {
+        let [.., x, _] = a;
+        debug_assert!(x.f == 30);
     } else {
-        let[_, _y..] = a;
+        let[_, y, ..] = a;
+        debug_assert!(y.f == 20);
     }
 }
