4818a7d3f4960e27d0584960f6bb2efc0bfa689e
Herman Venter <hermanv@fb.com>
2019-04-03 15:14:19 -0700

    Turn call to core.slice.len into lookup of ArrayLength path.

---
+++ checker/tests/run-pass/match_guard.rs
@@ -0,0 +1,27 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that uses a match guard to check that an index is within bounds
+
+pub enum Path {
+    LocalVariable { ordinal: usize },
+}
+
+pub struct AbstractValue {}
+
+impl Path {
+    pub fn refine_parameters(&self, arguments: &[(i32, AbstractValue)]) -> i32 {
+        match self {
+            Path::LocalVariable { ordinal } if 0 < *ordinal && *ordinal <= arguments.len() => {
+                arguments[*ordinal - 1].0 //~ possible attempt to subtract with overflow
+                                          //~ possible array index out of bounds
+            }
+            _ => 0,
+        }
+    }
+}
+
+pub fn main() {}
