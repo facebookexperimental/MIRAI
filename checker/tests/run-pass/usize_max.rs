9385a8a446b0e156fdfae3eb162cd02715737209
Darion Cassel <darioncassel@fb.com>
2019-06-21 13:39:32 -0700

    Add u64::MAX contract and test

---
+++ checker/tests/run-pass/usize_max.rs
@@ -0,0 +1,23 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that checks that size_of::<u32>() is mangled correctly.
+#![allow(non_snake_case)]
+
+#[macro_use]
+extern crate mirai_annotations;
+
+pub fn main() {
+    verify!(std::u64::MAX == 9223372036854775807);
+}
+
+pub mod foreign_contracts {
+    pub mod core {
+        pub mod u64 {
+            pub const MAX: u64 = 9223372036854775807;
+        }
+    }
+}
