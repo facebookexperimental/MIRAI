d6c8307e029d336e8c2b1c5c6edf68d647e0aced
Rashmi Mudduluru <rashmimudduluru@fb.com>
2019-06-26 18:27:55 -0700

    adding example: selection sort

---
+++ examples/selection_sort/src/main.rs
@@ -0,0 +1,25 @@
+fn selection_sort(array: &mut [i32]) {
+    let mut min;
+
+    for i in 0..array.len() {
+        min = i;
+
+        for j in (i + 1)..array.len() {
+            if array[j] < array[min] {
+                min = j;
+            }
+        }
+
+        let tmp = array[i];
+        array[i] = array[min];
+        array[min] = tmp;
+    }
+}
+
+fn main() {
+    let mut array = [9, 4, 8, 3, -5, 2, 1, 6];
+    println!("The initial array is {:?}", array);
+
+    selection_sort(&mut array);
+    println!(" The sorted array is {:?}", array);
+}
