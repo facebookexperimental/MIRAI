5f375d9ea834aca1275d3e9510944ae170e01b64
Herman Venter <hermanv@fb.com>
2019-07-08 14:29:00 -0700

    Give a warning if an explicit assumption is provably true. (#225)

---
+++ checker/tests/run-pass/true_assumption.rs
@@ -0,0 +1,24 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that uses a true assumption
+
+#[macro_use]
+extern crate mirai_annotations;
+
+pub fn test_assume() {
+    let i = 1;
+    assume!(i == 1); //~ assumption is provably true and can be deleted
+}
+
+pub fn test_unreachable_assume() {
+    let i = 1;
+    if i != 1 {
+        assume!(i == 1); //~ this is unreachable, mark it as such by using the unreachable! macro
+    }
+}
+
+pub fn main() {}
