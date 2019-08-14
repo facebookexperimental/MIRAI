ddc796a3ffadec40368c94c22c3ff23be9195c19
Darion Cassel <darioncassel@fb.com>
2019-08-14 16:55:42 -0700

    Add contracts to handle vec\![] macro (#266)

---
+++ checker/tests/run-pass/vec_empty_postcondition.rs
@@ -0,0 +1,29 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that uses built-in contracts for the Vec struct.
+
+#[macro_use]
+extern crate mirai_annotations;
+
+struct Test {
+    v: Vec<u8>,
+}
+
+impl Test {
+    fn new() -> Self {
+        let res = Self {
+            v: vec![],
+        };
+        assumed_postcondition!(res.v.is_empty());
+        res
+    }
+}
+
+pub fn main() {
+    let t = Test::new();
+    verify!(t.v.is_empty());
+}
