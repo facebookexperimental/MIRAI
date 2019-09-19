ca400d7f13d8b118ecf0341423d78223175ef37d
Darion Cassel <darioncassel@fb.com>
2019-09-19 16:31:28 -0700

    Add contracts for std::collections::VecDeque

---
+++ checker/tests/run-pass/vecdeque_pop_back.rs
@@ -0,0 +1,22 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test for VecDeque::pop_back
+
+#[macro_use]
+extern crate mirai_annotations;
+
+use std::collections::VecDeque;
+
+pub fn main() {
+    let mut v: VecDeque<i32> = VecDeque::new();
+    let old_len = v.len();
+    verify!(old_len == 0);
+    v.push_back(1);
+    verify!(v.len() == old_len + 1);
+    v.pop_back();
+    verify!(v.len() == old_len);
+}
---
+++ checker/tests/run-pass/vecdeque_push_front.rs
@@ -0,0 +1,20 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test for VecDeque::push_front
+
+#[macro_use]
+extern crate mirai_annotations;
+
+use std::collections::VecDeque;
+
+pub fn main() {
+    let mut v: VecDeque<i32> = VecDeque::new();
+    let old_len = v.len();
+    verify!(old_len == 0);
+    v.push_front(0);
+    verify!(v.len() == old_len + 1);
+}
