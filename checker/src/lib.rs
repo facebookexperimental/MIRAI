c769c4938ac114728795e6c8f4458bfe0c159790
Herman Venter <hermanv@fb.com>
2019-11-01 16:34:26 -0700

    Adapt to new nightly compiler

--- checker/src/lib.rs
+++ checker/src/lib.rs
@@ -22,7 +22,7 @@ extern crate rustc_data_structures;
 extern crate rustc_driver;
 extern crate rustc_errors;
 extern crate rustc_interface;
-extern crate rustc_metadata;
+extern crate rustc_typeck;
 extern crate syntax;
 extern crate syntax_pos;
 
