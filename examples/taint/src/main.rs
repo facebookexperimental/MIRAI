9e36dab024d33cb94e6654cb02330507b8cca238
Herman Venter <hermanv@fb.com>
2019-12-04 10:36:14 -0800

    Add license headers

--- examples/taint/src/main.rs
+++ examples/taint/src/main.rs
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
 extern crate taint_error;
 use std::sync::Arc;
 use taint_error::{source, use_arc, Foo};
