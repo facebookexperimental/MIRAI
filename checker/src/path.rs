03805f8da9e9aa26ddff148046ff7f44d92af705
Di Wang <diwang95@fb.com>
2020-06-11 16:10:15 -0400

    Remove deref selector when using Path::implicit_dereference

--- checker/src/path.rs
+++ checker/src/path.rs
@@ -92,23 +92,31 @@ impl Path {
     /// they are already canonical and because doing so can lead to recursive loops as refined paths
     /// re-introduce joined paths that have been split earlier.
     /// A split path, however, can be serve as the qualifier of a newly constructed qualified path
-    /// and this path might not be canonical. This routine removes the two sources of
+    /// and this path might not be canonical. This routine tries to remove the two sources of
     /// de-canonicalization that are currently know. Essentially: when a path that binds to a value
     /// that is a reference is implicitly dereferenced by the qualifier, the canonical path will
     /// be the one without the reference, or the the actual heap block, if the path binds to a heap
-    /// location.
+    /// location. This routine returns a re-canonicalized path in the two scenarios above,
+    /// otherwise returns `None`.
+    ///
+    /// If the function returns a re-canonicalized path, which is used as the qualifier of a newly
+    /// constructed qualified path, the caller of this function should check if the selector of the
+    /// qualified path is `PathSelector::Deref`. If so, the deref selector should also be removed.
     #[logfn_inputs(DEBUG)]
-    pub fn implicit_dereference(path: Rc<Path>, environment: &Environment) -> Rc<Path> {
+    pub fn try_implicit_dereference(
+        path: &Rc<Path>,
+        environment: &Environment,
+    ) -> Option<Rc<Path>> {
         if let PathEnum::Alias { value } = &path.value {
             if let Expression::Reference(path) = &value.expression {
-                return path.clone();
+                return Some(path.clone());
             }
-        } else if let Some(value) = environment.value_map.get(&path) {
+        } else if let Some(value) = environment.value_map.get(path) {
             if let Expression::HeapBlock { .. } = &value.expression {
-                return Path::get_as_path(value.clone());
+                return Some(Path::get_as_path(value.clone()));
             }
         }
-        path
+        None
     }
 }
 
