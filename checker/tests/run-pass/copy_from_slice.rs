5b78f8d7973f619e4307f7f26b5b35890b0a374a
Herman Venter <hermanv@fb.com>
2020-06-25 13:08:07 -0700

    Fix copy_from_slice. (#549)
    * Fix copy_from_slice.
    
    * Update body_visitor.rs

--- checker/tests/run-pass/copy_from_slice.rs
+++ checker/tests/run-pass/copy_from_slice.rs
@@ -3,6 +3,9 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
+#![feature(core_intrinsics)]
+use mirai_annotations::*;
+
 // A test that does a slice to slice copy where the underlying storage of the target slice is
 // not the same length as the source slice.
 
@@ -11,6 +14,14 @@ pub fn to_bytes(r: [u8; 4]) -> [u8; 8] {
 
     signature_bytes[..4].copy_from_slice(&r[..]);
     signature_bytes
+}
+
+pub fn test() {
+    let r = [1u8; 4];
+    let mut signature_bytes: [u8; 8] = [0u8; 8];
+
+    signature_bytes[..4].copy_from_slice(&r[..]);
+    verify!(signature_bytes[0] == 1);
 }
 
 pub fn main() {}
--- checker/tests/run-pass/slice_copy.rs
+++ checker/tests/run-pass/slice_copy.rs
@@ -10,31 +10,31 @@
 extern crate mirai_annotations;
 
 pub fn t1() {
-    let mut a = [1, 2, 3];
+    let a = [1, 2, 3];
     let mut b = [4, 5, 6];
     unsafe {
-        let aptr = a.as_mut_ptr();
+        let aptr = a.as_ptr();
         verify!(*aptr == 1);
         let bptr = b.as_mut_ptr();
         std::intrinsics::copy_nonoverlapping(aptr, bptr, 2);
     }
-    verify!(a[0] == 4);
-    verify!(a[1] == 5);
-    verify!(a[2] == 3);
+    verify!(b[0] == 1);
+    verify!(b[1] == 2);
+    verify!(b[2] == 6);
 }
 
-fn t2copy(a: &mut [i32], b: &mut [i32]) {
+fn t2copy(a: &[i32], b: &mut [i32]) {
     unsafe {
-        let aptr = a.as_mut_ptr();
+        let aptr = a.as_ptr();
         let bptr = b.as_mut_ptr();
         std::intrinsics::copy_nonoverlapping(aptr, bptr, 2);
     }
 }
 
 pub fn t2() {
-    let mut a = [1, 2, 3];
+    let a = [1, 2, 3];
     let mut b = [4, 5, 6];
-    t2copy(&mut b, &mut a);
+    t2copy(&a, &mut b);
     verify!(a[0] == 1);
     verify!(a[1] == 2);
     verify!(a[2] == 3);
@@ -43,18 +43,18 @@ pub fn t2() {
     verify!(b[2] == 6);
 }
 
-fn t3copy(a: &mut [i32], b: &mut [i32], count: usize) {
+fn t3copy(a: &[i32], b: &mut [i32], count: usize) {
     unsafe {
-        let aptr = a.as_mut_ptr();
+        let aptr = a.as_ptr();
         let bptr = b.as_mut_ptr();
         std::intrinsics::copy_nonoverlapping(aptr, bptr, count);
     }
 }
 
 pub fn t3() {
-    let mut a = [1, 2, 3];
+    let a = [1, 2, 3];
     let mut b = [4, 5, 6];
-    t3copy(&mut b, &mut a, 2);
+    t3copy(&a, &mut b, 2);
     verify!(a[0] == 1);
     verify!(a[1] == 2);
     verify!(a[2] == 3);
