7a078cde2d5e3a7a8e6cfec892ec3f6f2f35d8d7
Herman Venter <hermanv@fb.com>
2020-07-27 10:06:28 -0700

    Fix transfer of array repeat values from summaries (#589)

--- checker/tests/run-pass/array_repeat.rs
+++ checker/tests/run-pass/array_repeat.rs
@@ -9,8 +9,46 @@
 #[macro_use]
 extern crate mirai_annotations;
 
-pub fn main() {
+pub fn test1() {
     let x = [1; 2];
     verify!(x[0] == 1);
     verify!(x[1] == 1);
 }
+
+fn foo() -> [i32; 2] {
+    [3; 2]
+}
+
+pub fn test2() {
+    let x = foo();
+    verify!(x[0] == 3);
+    verify!(x[1] == 3);
+}
+
+fn bar(a: &mut [i32; 2]) {
+    *a = [4; 2];
+}
+
+pub fn test3() {
+    let mut x = [1; 2];
+    bar(&mut x);
+    verify!(x[0] == 4);
+    verify!(x[1] == 4);
+}
+
+pub fn test4() {
+    let mut x = [1, 2];
+    bar(&mut x);
+    verify!(x[0] == 4);
+    verify!(x[1] == 4);
+}
+
+pub fn test5() {
+    let mut x = [1; 2];
+    x[0] = 3;
+    bar(&mut x);
+    verify!(x[0] == 4);
+    verify!(x[1] == 4);
+}
+
+pub fn main() {}
