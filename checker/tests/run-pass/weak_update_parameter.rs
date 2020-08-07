e141dacb7e083361b6fa9cd22f7bf3831f1419f2
Herman Venter <hermanv@fb.com>
2020-08-07 10:37:08 -0700

    Quick fix for issue #603 (#605)

---
+++ checker/tests/run-pass/weak_update_parameter.rs
@@ -0,0 +1,23 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that assigns to an unknown index of a mutable array parameter
+
+#[macro_use]
+extern crate mirai_annotations;
+
+fn test(a: &mut [i32; 4], i: usize) {
+    a[i] = 666;
+}
+
+pub fn main() {
+    let mut v = [1, 2, 3, 4];
+    let i = abstract_value!(1);
+    assume!(i < 4);
+    test(&mut v, i);
+    verify!(v[1] == 2 || v[1] == 666);
+    verify!(v[1] == 666); //~ possible false verification condition
+}
