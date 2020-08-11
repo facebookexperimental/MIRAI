1f81f4c90e448acb674c31ad77f5e693174f9b9f
JIexa <malyshevalexey8@gmail.com>
2020-08-11 00:28:42 -0500

    Fixed the test

--- checker/tests/run-pass/assume_provably_false.rs
+++ checker/tests/run-pass/assume_provably_false.rs
@@ -1,5 +1,17 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test that checks that a provably false assumption is ignored in the assertion.
+
+
+#[macro_use]
+extern crate mirai_annotations;
+
 pub fn main() {
     let a = 5;
-    assume!(a < 5); //this assumption is provably false and it will be ignored
+    assume!(a < 5); //~ assumption is provably false and it will be ignored in the assertion
     verify!(a == 5);
 }
