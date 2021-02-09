a87721fa55948c422e4193658147bbb102df7430
Herman Venter <hermanv@fb.com>
2021-02-08 18:59:32 -0800

    Weaken index paths (#751)

---
+++ checker/tests/run-pass/read_to_end.rs
@@ -0,0 +1,19 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+// Tests that Cursor::read_to_end havocs the length of its parameter vector.
+
+use mirai_annotations::*;
+use std::io::{Cursor, Read, Result};
+
+pub fn t1(buf: &[u8]) -> Result<()> {
+    let mut reader = Cursor::new(buf);
+    let mut v = Vec::with_capacity(1);
+    reader.read_to_end(&mut v)?;
+    verify!(v.len() == 0); //~ possible false verification condition
+    Ok(())
+}
+
+pub fn main() {}
