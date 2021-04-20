c355fa1817445906c8628b4b497d2ff94899d36f
Herman Venter <hermanv@fb.com>
2021-04-20 11:47:59 -0700

    Add rule: [(x || (y && !z)) || (y && z)))] -> x || y (#826)

--- checker/tests/run-pass/bit_vector_and_vec_push.rs
+++ checker/tests/run-pass/bit_vector_and_vec_push.rs
@@ -6,7 +6,7 @@
 
 // A test that uses bit vectors in the SMT solver
 
-//use mirai_annotations::*;
+use mirai_annotations::*;
 
 pub fn write_u32_as_uleb128(binary: &mut Vec<u8>, value: u32) {
     let mut val = value;
@@ -29,8 +29,8 @@ pub fn write_u32_as_uleb128(binary: &mut Vec<u8>, value: u32) {
 }
 
 pub fn main() {
-    // let mut buf = Vec::<u8>::new();
-    // write_u32_as_uleb128(&mut buf, 129);
-    // verify!(buf.len() == 2); // ~ possible false verification condition
-    // verify!(buf.len() == 1); // ~ provably false verification condition
+    let mut buf = Vec::<u8>::new();
+    write_u32_as_uleb128(&mut buf, 129);
+    verify!(buf.len() == 2); //~ possible false verification condition
+    verify!(buf.len() == 1); //~ provably false verification condition
 }
