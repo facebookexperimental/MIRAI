e452457553592478893b08cf2d62db154d271e40
Herman Venter <hermanv@fb.com>
2021-04-29 14:53:13 -0700

    Add license headers

--- standard_contracts/src/macros.rs
+++ standard_contracts/src/macros.rs
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
 macro_rules! add_with_overflow {
     ($t:ty, $tt:ty, $n:ident, $m:expr ) => {
         pub fn $n(x: $t, y: $t) -> ($tt, bool) {
