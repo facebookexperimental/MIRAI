5c85b8e1388ae4ee20879e43e75e2f2ce6d712be
Herman Venter <hermanv@fb.com>
2021-07-02 18:28:50 -0700

    Check path prefixes when checking a subcomponent of an abstract local (#903)

---
+++ checker/tests/run-pass/tag_result.rs
@@ -0,0 +1,39 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+// A test for adding tags to Result structs
+
+#![feature(const_generics)]
+#![allow(incomplete_features)]
+
+use mirai_annotations::*;
+
+struct SecretTaintKind<const MASK: TagPropagationSet> {}
+
+type SecretTaint = SecretTaintKind<TAG_PROPAGATION_ALL>;
+
+fn get_abs_vec_val() -> Result<Vec<u8>, ()> {
+    result!()
+}
+
+pub fn test1() -> Result<(), ()> {
+    let x: Result<Vec<u8>, ()> = get_abs_vec_val();
+    add_tag!(&x, SecretTaint);
+    let y = x?;
+    verify!(has_tag!(&y, SecretTaint));
+    Ok(())
+}
+
+pub fn test2() {
+    let x: Result<i32, ()> = Ok(42);
+    add_tag!(&x, SecretTaint);
+    let y = x.or(Err(()));
+    verify!(does_not_have_tag!(&y, SecretTaint)); // tag does not flow to y
+    if let Ok(z) = y {
+        verify!(has_tag!(&z, SecretTaint)); // but it does flow to y.Ok
+    }
+}
+
+pub fn main() {}
