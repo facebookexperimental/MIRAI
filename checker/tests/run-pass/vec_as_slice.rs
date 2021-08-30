fa6a62819d386f76d743a5f8698fc8bd3dc5b40c
Herman Venter <hermanv@fb.com>
2021-08-30 11:35:49 -0700

    Add some shelved tests to active suite

---
+++ checker/tests/run-pass/vec_as_slice.rs
@@ -0,0 +1,33 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// This tests de-referencing a vector inside a transparent wrapper
+
+use mirai_annotations::*;
+
+#[repr(transparent)]
+pub struct Buf {
+    pub inner: Vec<u8>,
+}
+
+#[repr(transparent)]
+pub struct Slice {
+    pub inner: [u8],
+}
+
+impl Buf {
+    pub fn as_slice(&self) -> &Slice {
+        unsafe { std::mem::transmute(&*self.inner) }
+    }
+}
+
+pub fn t1() {
+    let b = Buf { inner: vec![10] };
+    let sl = b.as_slice();
+    verify!(sl.inner.len() == 1);
+}
+
+pub fn main() {}
