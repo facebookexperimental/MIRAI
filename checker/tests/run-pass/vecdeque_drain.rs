e870e45968b766a52d6f13e9f451332e5e12c81b
Herman Venter <hermanv@fb.com>
2021-12-14 17:49:26 -0800

    Support pass-through functions (#1111)

---
+++ checker/tests/run-pass/vecdeque_drain.rs
@@ -0,0 +1,15 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// A test for VecDeque::drain
+
+use std::{collections::VecDeque, sync::Mutex};
+
+pub fn main() {
+    let q: Mutex<VecDeque<i32>> = Mutex::new(VecDeque::with_capacity(10_000));
+    let mut dq = q.lock().unwrap();
+    let _v: Vec<i32> = dq.drain(..).collect();
+}
