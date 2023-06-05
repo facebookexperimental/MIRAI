b77cf5075f3c69a619c32555002cba77ef7f0646
Thomas Niederberger <thomas@niederb.ch>
2023-06-05 10:27:05 +0200

    Use contracts and add some comments

--- substrate-examples/offchain-worker/src/lib.rs
+++ substrate-examples/offchain-worker/src/lib.rs
@@ -70,6 +70,7 @@ use sp_runtime::{
     RuntimeDebug,
 };
 use sp_std::vec::Vec;
+use contracts::*;
 
 mod tests;
 
@@ -347,9 +348,16 @@ pub mod pallet {
                 if !signature_valid {
                     return InvalidTransaction::BadProof.into()
                 }
-                Self::validate_transaction_parameters(&payload.block_number, &payload.price)
+                let res = Self::validate_transaction_parameters(&payload.block_number, &payload.price);
+                // Properly check that the transaction is valid before applying the data
+                if res.is_ok() {
+                    Self::apply_data(&payload.block_number, &payload.price);
+                }
+                
+                res
             } else if let Call::submit_price_unsigned { block_number, price: new_price } = call {
                 let res = Self::validate_transaction_parameters(block_number, new_price);
+                // Applying the data without properly checking that the parameters were validated correctly
                 Self::apply_data(block_number, new_price);
                 res
             } else {
@@ -699,12 +707,14 @@ impl<T: Config> Pallet<T> {
         }
     }
 
+    // Make sure that the parameters were verified before we apply the data
+    #[requires(has_tag!(new_price, ParameterVerified))]
+    #[requires(has_tag!(block_number, ParameterVerified))]
     fn apply_data(
         block_number: &T::BlockNumber,
         new_price: &u32,
     ) {
-        precondition!(has_tag!(new_price, ParameterVerified));
-        precondition!(has_tag!(block_number, ParameterVerified));
+
     }
 
     fn validate_transaction_parameters(
